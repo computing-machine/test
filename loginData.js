64d32f1dd8895cbd0254b16d49964e626819e484
umo <almalik.umair@gmail.com>
2018-06-04 07:46:26 +0500

    Pages rendered dynamic but still not working.

---
+++ loginData.js
@@ -0,0 +1,4 @@
+var data = {
+    "input":[{"id": "username", "name":"username", "type":"text", "placeholder":"username"},
+            {"id": "password", "name":"password", "type":"password", "placeholder":"password"}]
+}
